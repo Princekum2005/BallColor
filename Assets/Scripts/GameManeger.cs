eb15717b16288d6cf8be5392958a88806b846611
Prince2005yadav <princekumyad2005@gmail.com>
2023-07-20 15:20:06 +0530

    ball selection

--- Assets/Scripts/GameManeger.cs
+++ Assets/Scripts/GameManeger.cs
@@ -9,6 +9,14 @@ public class GameManeger : MonoBehaviour
     private GameObject mainPanel;
     [SerializeField]
     private GameObject finishPanel;
+    [SerializeField]
+    private GameObject changeBallPanel;
+    [SerializeField]
+    private GameObject redImage;
+    [SerializeField]
+    private GameObject blueImage;
+    [SerializeField]
+    private BallDeta ballDeta;
     // Start is called before the first frame update
 
     private void Awake()
@@ -16,15 +24,38 @@ public class GameManeger : MonoBehaviour
         Time.timeScale = 0f;
         mainPanel.SetActive(true);
         finishPanel.SetActive(false);
+        changeBallPanel.SetActive(false);
+        ballDeta.redBall.SetActive(false);
     }
     public void PlayButton()
     {
         mainPanel.SetActive(false);
+        changeBallPanel.SetActive(false);
         Time.timeScale = 1f;
+    }
+    public void ChangeButton()
+    {
+        changeBallPanel.SetActive(true);
+        mainPanel.SetActive(false);
+    }
+    public void BlueButton()
+    {
+        redImage.SetActive(false);
+        blueImage.SetActive(true);
+        ballDeta.redBall.SetActive(false);
+        ballDeta.blueBall.SetActive(true);
+    }
+    public void RedButton()
+    {
+        redImage.SetActive(true);
+        blueImage.SetActive(false);
+        ballDeta.redBall.SetActive(true);
+        ballDeta.blueBall.SetActive(false);
     }
     public void ReplayButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+    
 
 }
---
+++ Assets/Scripts/deta/BallDeta.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDeta : MonoBehaviour
+{
+    public GameObject redBall;
+    public GameObject blueBall;
+}
